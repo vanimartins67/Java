aa3303d0c5699ed3fe5d87bc18fc7def9c16f733
vanimartins67 <vmartins.ti@gmail.com>
2025-08-04 19:53:09 -0400

    .

---
+++ src/Exerc2.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Exerc2 {
+
+    public static void main(String[] args) {
+
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Digite um número: ");
+    int num = scanner.nextInt();
+
+    if (num > 10){
+        System.out.println("O número é maior do que 10.");
+    }
+    else if (num <= 10){
+        System.out.println("O número é menor ou igual a 10.");
+    }
+
+    scanner.close();
+    
+}
+}
